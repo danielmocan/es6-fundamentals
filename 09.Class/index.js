e8d5a22fb1d7ab7dea7dac94cd020df55f05122a
Daniel Mocan <contact_daniel@yahoo.com>
2021-07-12 00:26:44 +0300

    Update index.js

--- 09.Class/index.js
+++ 09.Class/index.js
@@ -16,3 +16,12 @@ class Employee extends Person {
 
 const employee = new Employee('Daniel', 'Mocan', '25', 'Operations', 1000);
 console.log(employee);
+
+/* Exercises */
+//1. Create a `MathOperations` class that implements the following static methods: `square`, `double`, `cube`. Use all of them on number 5.
+
+/* 2. Create a class `Person` that has the following properties: `firstName`, `lastName`,`age` and a method `fullName`.
+From this class create other two subclasses: `Student` and `Teacher`.
+`Stundent`gets extra properties `year`,`grades` (array) and a method `showGrades`;
+`Teacher` gets extra properties `courses`,`department` and a method `showTeacherInfo` that prints all the details for the teacher;
+*/
